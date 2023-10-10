6fb655e504bcf291a212d30444d14f168184eb58
seven <seven@diqigan.cn>
2023-10-11 00:44:29 +0800

    update meta info

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 - [篡改猴](https://www.tampermonkey.net/) | [Doc](https://www.tampermonkey.net/documentation.php)
 - [GreasyFork](https://greasyfork.org/) 篡改猴脚本市场
+
+## TOC
+
+- [复制 Notion Page 内容为 Markdown 文本](./copy-notion-page-content-as-markdown/)
+- [外链自动跳转](./external-link-auto-jump/)
--- copy-notion-page-content-as-markdown/copy-notion-page-content-as-markdown.js
+++ copy-notion-page-content-as-markdown/copy-notion-page-content-as-markdown.js
@@ -5,13 +5,14 @@
 // @description  复制 Notion Page 内容为标准 Markdown 文本。
 // @description:zh-CN  复制 Notion Page 内容为标准 Markdown 文本。
 // @description:en Copy Notion Page Content AS Markdown.
-// @namespace    https://blog.diqigan.cn
+// @namespace https://github.com/Seven-Steven/tampermonkey-scripts/blob/main/copy-notion-page-content-as-markdown
 // @version      0.1.1
-// @license MIT
 // @author       Seven
 // @match        *://www.notion.so/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=notion.so
 // @grant        GM_setClipboard
+// @supportURL https://github.com/Seven-Steven/tampermonkey-scripts/issues
+// @license MIT
 // ==/UserScript==
 
 (function () {
